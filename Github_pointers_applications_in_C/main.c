de770b3f0d3e66418eb7e7df860cba5028f3e14d
Dhruv Dave <dhruvdave176@gmail.com>
2022-03-05 02:14:29 +0530

    Initial Commit

---
+++ Github_pointers_applications_in_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Github_pointers_applications_in_C
+//
+//  Created by Dhruv Dave on 05/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
